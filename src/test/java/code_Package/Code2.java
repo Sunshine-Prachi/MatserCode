3277410beb8917e61c96c66ea684e65b2e975fb9
Sunshine-Prachi <prachirathod1764@gmail.com>
2024-06-16 23:48:10 +0530

    code2 and code3 added

---
+++ src/test/java/code_Package/Code2.java
@@ -0,0 +1,10 @@
+package code_Package;
+
+public class Code2 {
+
+	public static void main(String[] args) {
+	System.out.println("Code2 running");
+
+	}
+
+}
---
+++ src/test/java/code_Package/Code3.java
@@ -0,0 +1,10 @@
+package code_Package;
+
+public class Code3 {
+
+	public static void main(String[] args) {
+	System.out.println("Code3 running");
+
+	}
+
+}
